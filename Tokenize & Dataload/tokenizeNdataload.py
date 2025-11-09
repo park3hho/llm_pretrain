483c22d6e8a344564b71367c9fec14232cff984c
park3hho <ejfhr7777@gmail.com>
2025-11-09 15:19:48 +0900

    0.0.4 Check Tokenization Code (0) 제곧내

--- Tokenize & Dataload/tokenizeNdataload.py
+++ Tokenize & Dataload/tokenizeNdataload.py
@@ -29,15 +29,16 @@ class MyDataset(Dataset):
 
         for i in range(0, len(token_ids) - max_length, stride):
             input_chunk = token_ids[i:i + max_length]
-            target_chunk = token_ids[i + 1: i + max_length + 1]
+            target_chunk = token_ids[i + 1: i + max_length + 1] # Next Token Prediction
+            # Input: 입력값, Target: 출력값, 입력값에 따라 출력값이 학습한 가중치에 따라 유사하게 나오게 만든다.
             self.input_ids.append(torch.tensor(input_chunk))
             self.target_ids.append(torch.tensor(target_chunk))
 
     def __len__(self):
-        return len(self.input_ids)
+        return len(self.input_ids) # token qunatities
 
     def __getitem__(self, idx):
-        return self.input_ids[idx], self.target_ids[idx]
+        return self.input_ids[idx], self.target_ids[idx] # input
 
 # with open("cleaned_한글문서.txt", 'r', encoding='utf-8-sig') as file: # 선택: -sig를 붙여서 BOM 제거
 with open("../DataSet/Cleaned_Dataset/cleaned_02 Harry Potter and the Chamber of Secrets.txt", 'r', encoding='utf-8-sig') as file: # 선택: -sig를 붙여서 BOM 제거
@@ -48,3 +49,12 @@ dataset = MyDataset(txt, max_length = 32, stride = 4)
 train_loader = DataLoader(dataset, batch_size=128, shuffle=True, drop_last=True)
 
 # 주의: 여기서는 코드를 단순화하기 위해 test, valid는 생략
+
+
+# Tokenization Check #
+# Batch refers to the batch of samples that are entered into the model at a time.
+# iterator: traversal object
+dataiter = iter(train_loader)
+x,y = next(dataiter)
+print(tokenizer.decode(x[0].tolist()))
+print(tokenizer.decode(y[0].tolist()))
